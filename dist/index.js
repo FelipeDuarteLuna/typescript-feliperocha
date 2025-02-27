87ff32aaa36149a153f947cfb6e330047fa777b3
Felipe Luna <felipinho.luna@gmail.com>
2025-02-27 09:17:38 -0300

    Add initial JavaScript and TypeScript files with age variables and console logs

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let age = 5;
+console.log(age);
+let age1 = "FelipeLuna";
+console.log(age1);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let age = 5;
+console.log(age);
+
+let age1: string = "FelipeLuna";
+console.log(age1);
